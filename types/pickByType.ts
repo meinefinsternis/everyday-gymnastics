de850b5600ff409164b553e70098dba8bd023382
meinefinsternis <87833239+meinefinsternis@users.noreply.github.com>
2022-07-14 00:02:20 +0300

    pick by type

---
+++ types/pickByType.ts
@@ -0,0 +1,23 @@
+import { Equal, Expect } from "../utils";
+
+type PickByType<T, U> = {
+  [K in keyof T as U extends T[K] ? K : never]: T[K];
+};
+
+interface Model {
+  name: string;
+  count: number;
+  isReadonly: boolean;
+  isEnable: boolean;
+}
+
+type cases = [
+  Expect<
+    Equal<
+      PickByType<Model, boolean>,
+      { isReadonly: boolean; isEnable: boolean }
+    >
+  >,
+  Expect<Equal<PickByType<Model, string>, { name: string }>>,
+  Expect<Equal<PickByType<Model, number>, { count: number }>>
+];
