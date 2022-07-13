753005e524f54790a41d7b6774c20ec9d817a316
meinefinsternis <87833239+meinefinsternis@users.noreply.github.com>
2022-07-14 00:24:19 +0300

    flatten

---
+++ types/flatten.ts
@@ -0,0 +1,20 @@
+import { Equal, Expect } from "../utils";
+
+type Flatten<T extends any[]> = T extends [infer Head, ...infer Rest]
+  ? Head extends unknown[]
+    ? [...Flatten<Head>, ...Flatten<Rest>]
+    : [Head, ...Flatten<Rest>]
+  : [];
+
+type cases = [
+  Expect<Equal<Flatten<[]>, []>>,
+  Expect<Equal<Flatten<[1, 2, 3, 4]>, [1, 2, 3, 4]>>,
+  Expect<Equal<Flatten<[1, [2]]>, [1, 2]>>,
+  Expect<Equal<Flatten<[1, 2, [3, 4], [[[5]]]]>, [1, 2, 3, 4, 5]>>,
+  Expect<
+    Equal<
+      Flatten<[{ foo: "bar"; 2: 10 }, "foobar"]>,
+      [{ foo: "bar"; 2: 10 }, "foobar"]
+    >
+  >
+];
