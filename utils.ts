05b4b1ce32a5dc1af476c0b5a166ec76ed97bb36
meinefinsternis <87833239+meinefinsternis@users.noreply.github.com>
2022-07-13 23:49:56 +0300

    init

---
+++ utils.ts
@@ -0,0 +1,13 @@
+export type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <
+  T
+>() => T extends Y ? 1 : 2
+  ? true
+  : false;
+
+export type Expect<T extends true> = T;
+
+export type MergeInsertions<T> = T extends object
+  ? { [K in keyof T]: MergeInsertions<T[K]> }
+  : T;
+
+export type Alike<X, Y> = Equal<MergeInsertions<X>, MergeInsertions<Y>>;
