83c1fe87c153461e8f9b0ce8182231c924060f3f
meinefinsternis <87833239+meinefinsternis@users.noreply.github.com>
2022-07-15 21:12:44 +0300

    drop char

---
+++ types/dropChar.ts
@@ -0,0 +1,15 @@
+import { Equal, Expect } from "../utils";
+
+type DropChar<
+  T extends string,
+  U extends string
+> = T extends `${infer H}${U}${infer R}` ? `${H}${DropChar<R, U>}` : T;
+
+type cases = [
+  Expect<Equal<DropChar<"butter fly!", " ">, "butterfly!">>,
+  Expect<Equal<DropChar<"butter fly!", "!">, "butter fly">>,
+  Expect<Equal<DropChar<"    butter fly!        ", " ">, "butterfly!">>,
+  Expect<Equal<DropChar<" b u t t e r f l y ! ", " ">, "butterfly!">>,
+  Expect<Equal<DropChar<" b u t t e r f l y ! ", "b">, "  u t t e r f l y ! ">>,
+  Expect<Equal<DropChar<" b u t t e r f l y ! ", "t">, " b u   e r f l y ! ">>
+];
