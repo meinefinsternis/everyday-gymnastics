eed3b137621a0a1256c247728a635919bb1907a8
meinefinsternis <87833239+meinefinsternis@users.noreply.github.com>
2022-07-17 17:04:34 +0300

    trim right

---
+++ types/trimRight.ts
@@ -0,0 +1,15 @@
+import { Equal, Expect } from "../utils";
+
+type TrimRight<T> = T extends `${infer Head}${" " | "\n" | "\t"}`
+  ? TrimRight<Head>
+  : T;
+
+type cases = [
+  Expect<Equal<TrimRight<"str">, "str">>,
+  Expect<Equal<TrimRight<"str ">, "str">>,
+  Expect<Equal<TrimRight<"str     ">, "str">>,
+  Expect<Equal<TrimRight<"     str     ">, "     str">>,
+  Expect<Equal<TrimRight<"   foo bar  \n\t ">, "   foo bar">>,
+  Expect<Equal<TrimRight<"">, "">>,
+  Expect<Equal<TrimRight<"\n\t ">, "">>
+];
